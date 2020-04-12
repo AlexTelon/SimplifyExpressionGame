3293129467e61d8c9d32c8a481fe27c84b568f5e
Alex Telon <alex.telon@gmail.com>
2020-04-13 00:05:25 +0200

    Added more interesting typing examples
    Also restructured a bit.

--- typing.py
+++ typing.py
@@ -1,46 +1,51 @@
 from pygametextinput import pygame_textinput
 import pygame as pg
-pg.init()
 
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+def get_next_example():
+    import random
+    with open('typing.py') as f:
+        lines = f.read().splitlines()
+        return random.choice(lines).strip()
 
-textinput = pygame_textinput.TextInput(antialias=True, font_family='Consolas', text_color=WHITE, cursor_color=WHITE)
+if __name__ == '__main__':
+    pg.init()
+    WHITE = (255, 255, 255)
 
-screen = pg.display.set_mode((1000, 200))
-clock = pg.time.Clock()
+    textinput = pygame_textinput.TextInput(antialias=True, font_family='Consolas', text_color=WHITE, cursor_color=WHITE)
 
-problem_description_font = pg.font.SysFont("Consolas", 30)
+    screen = pg.display.set_mode((1000, 200))
+    clock = pg.time.Clock()
 
-example_text = "if this"
+    problem_description_font = pg.font.SysFont("Consolas", 30)
 
-def get_user_input():
-    return textinput.input_string
-
-while True:
-    screen.fill(BLACK)
-
-    events = pg.event.get()
-    for event in events:
-        if event.type == pg.QUIT:
-            exit()
-
-    keys = pg.key.get_pressed()
-
-    if keys[pg.K_KP_ENTER]:
-        if textinput.input_string == example_text:
-            textinput.input_string = ""
-            example_text += " " + example_text
-        if textinput.input_string == "quit":
-            exit()
-    else:
-        textinput.update(events)
-        screen.blit(textinput.get_surface(), (10, 10))
+    example_text = get_next_example()
 
 
-    label = problem_description_font.render(example_text, 1, WHITE)
-    screen.blit(label, (10, 100))
+    while True:
+        screen.fill(BLACK)
+
+        events = pg.event.get()
+        for event in events:
+            if event.type == pg.QUIT:
+                exit()
+
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_KP_ENTER]:
+            if textinput.input_string == example_text:
+                textinput.input_string = ""
+                example_text = get_next_example()
+            if textinput.input_string == "quit":
+                exit()
+        else:
+            textinput.update(events)
+            screen.blit(textinput.get_surface(), (10, 10))
 
 
-    pg.display.update()
-    clock.tick(30)
+        label = problem_description_font.render(example_text, 1, WHITE)
+        screen.blit(label, (10, 100))
+
+
+        pg.display.update()
+        clock.tick(30)
